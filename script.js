5e855db898f5d796787ddcabbf60a862451e698d
Donutask <51319000+Donutask@users.noreply.github.com>
2022-02-07 20:47:35 +1100

    Web app install??

--- script.js
+++ script.js
@@ -86,3 +86,34 @@ document.onload = new function () {
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('script.js');
 };
+
+//Web app install
+//https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps/Add_to_home_screen
+let deferredPrompt;
+const addButton = document.getElementById("add_button");
+addButton.style.display = 'none';
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+    // Update UI to notify the user they can add to home screen
+    addButton.style.display = 'block';
+
+    addButton.addEventListener('click', (e) => {
+        // hide our user interface that shows our A2HS button
+        addButton.style.display = 'none';
+        // Show the prompt
+        deferredPrompt.prompt();
+        // Wait for the user to respond to the prompt
+        deferredPrompt.userChoice.then((choiceResult) => {
+            if (choiceResult.outcome === 'accepted') {
+                // console.log('User accepted the A2HS prompt');
+            } else {
+                // console.log('User dismissed the A2HS prompt');
+            }
+            deferredPrompt = null;
+        });
+    });
+});
